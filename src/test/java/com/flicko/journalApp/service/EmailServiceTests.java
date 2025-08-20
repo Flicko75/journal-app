7785d6a3f0da1b1b76ca4202d3dc35bced2556a8
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-08-20 21:16:30 +0530

    email service added

---
+++ src/test/java/com/flicko/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,18 @@
+package com.flicko.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail(){
+        emailService.sendEmail("hello@gmail.com","Testing mail","Hello bro");
+    }
+
+}
